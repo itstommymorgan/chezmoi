0861d65d355a26c0d34e72316ee8b8f7cbb9aa61
Tommy Morgan <tommy@tommymorgan.name>
2026-08-23 08:40:08 -0400

    Replace cmp.nvim with blink.cmp

--- dot_config/nvim/lua/plugins/coding_ui.lua
+++ dot_config/nvim/lua/plugins/coding_ui.lua
@@ -35,10 +35,7 @@ return {
     'windwp/nvim-autopairs',
     after = {'nvim-treesitter', 'nvim-cmp'},
     config = function()
-      require('nvim-autopairs').setup()
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      local cmp = require('cmp')
-      cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+      require('nvim-autopairs').setup({})
     end,
   },
 
