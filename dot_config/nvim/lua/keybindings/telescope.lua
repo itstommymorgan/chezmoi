062606c5169fe498f207ae6e760e2942584494d8
Tommy Morgan <tommy@tommymorgan.name>
2026-08-23 22:13:29 -0400

    Remove unnecessary duplicate config for telescope

--- dot_config/nvim/lua/keybindings/telescope.lua
+++ dot_config/nvim/lua/keybindings/telescope.lua
@@ -1,39 +1,5 @@
 local config = require("config")
 
-local actions = require("telescope.actions")
-local trouble = require("trouble.sources.telescope")
-
-require("telescope").setup({
-	defaults = {
-		mappings = {
-			i = {
-				["<c-t>"] = trouble.open,
-				["<esc>"] = actions.close,
-			},
-			n = {
-				["<c-t>"] = trouble.open,
-			},
-		},
-		vimgrep_arguments = {
-			"rg",
-			"--hidden",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-		},
-	},
-	pickers = {
-		find_files = {
-			hidden = true,
-		},
-	},
-})
-
-require("telescope").load_extension("fzf")
-
 config.nmap("<Leader>f<Leader>", "<cmd>Telescope grep_string<cr>")
 config.nmap("<Leader>fb", "<cmd>Telescope buffers theme=ivy<cr>")
 config.nmap("<Leader>ff", "<cmd>Telescope find_files theme=ivy<cr>")
