362d58bebadbe91189f8b5eff0effc3aaeffe42a
Tommy Morgan <tommy@tommymorgan.name>
2026-08-23 21:18:03 -0400

    Drop quickfix-reflector for quicker.nvim

--- dot_config/nvim/lua/plugins/coding_ui.lua
+++ dot_config/nvim/lua/plugins/coding_ui.lua
@@ -52,8 +52,29 @@ return {
 		config = true,
 	},
 
-	-- Allow you to edit files directly from the quickfix results
-	"stefandtw/quickfix-reflector.vim",
+	-- better quickfix
+	{
+		"stevearc/quicker.nvim",
+		ft = "qf",
+		opts = {
+			keys = {
+				{
+					">",
+					function()
+						require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+					end,
+					desc = "Expand quickfix context",
+				},
+				{
+					"<",
+					function()
+						require("quicker").collapse()
+					end,
+					desc = "Collapse quickfix context",
+				},
+			},
+		},
+	},
 
 	-- Navigation breadcrumbs (for lualine)
 	{
