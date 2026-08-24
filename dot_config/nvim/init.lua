bc2e2796031921702b90137bc910c180e4c204b3
Tommy Morgan <tommy@tommymorgan.name>
2026-08-23 21:43:33 -0400

    Remove outdated require

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,10 +1,9 @@
 -- First, draw some circles
-require('draw_some_circles')
+require("draw_some_circles")
 
 --then, draw the rest of the owl
-require('plugin_manager')
-require('display')
-require('line_numbers')
-require('editing')
-require('keybindings')
-require('lsp')
+require("plugin_manager")
+require("display")
+require("line_numbers")
+require("editing")
+require("keybindings")
